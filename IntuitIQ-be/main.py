cb30013cff6e0786295bcc690b6cee9e858a8757
aruproyy <aruproy299@gmail.com>
2025-04-27 01:48:46 +0530

    frontend update

--- IntuitIQ-be/main.py
+++ IntuitIQ-be/main.py
@@ -15,7 +15,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=[
+        "https://intuitiq-v2.netlify.app",  # Your Netlify URL
+        "http://localhost:3000",                  # For local development
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
